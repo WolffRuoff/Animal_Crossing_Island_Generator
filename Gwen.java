6e1698954173d657ca9c129a4f59186dc98a0fee
gwnch <gwendolyn.chu@gmail.com>
2020-04-16 23:08:37 -0700

    Little changes to hopefully make it easier for you ha

--- Gwen.java
+++ Gwen.java
@@ -10,7 +10,7 @@ public class Gwen {
 		TileMap mappy = new TileMap(112,112);
 		
 		// water layer by ethan
-		BufferedImage water = Utility.LoadImage(".//Resources//water.png");
+		BufferedImage water = Utility.LoadImage(".\\Resources\\water.png");
 		for(int i = 0; i < mappy.getWidth(); i++) {
 			for (int j = 0; j < mappy.getHeight(); j++) {
 				mappy.setTile(i, j, water);
@@ -18,14 +18,14 @@ public class Gwen {
 		}
 		
 		// sand layer
-		BufferedImage sand = Utility.LoadImage(".//Resources//Sand.png");
+		BufferedImage sand = Utility.LoadImage(".\\Resources\\Sand.png");
 		SandGrass.south(13, 13, 20, 20, mappy, sand);
 		SandGrass.east(12, 13, 90, 90, mappy, sand);
 		SandGrass.west(12, 13, 3, 20, mappy, sand);
 		SandGrass.fill(20, 90, 12, 20, mappy, sand);
 
 		// ground grass layer
-		BufferedImage groundGrass = Utility.LoadImage(".//Resources//GroundGrass.png");
+		BufferedImage groundGrass = Utility.LoadImage(".\\Resources\\GroundGrass.png");
 		SandGrass.south(20, 20, 80, 80, mappy, groundGrass);
 		SandGrass.east(12, 18, 70, 80, mappy, groundGrass);
 		SandGrass.west(12, 18, 10, 40, mappy, groundGrass);
--- Main.java
+++ Main.java
@@ -5,28 +5,5 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Fuck This!!!!!");
-		
-		TileMap mappy = new TileMap(112,112);
-		
-		// water layer
-		BufferedImage water = Utility.LoadImage(".\\Resources\\water.png");
-		for(int i = 0; i < mappy.getWidth(); i++) {
-			for (int j = 0; j < mappy.getHeight(); j++) {
-				mappy.setTile(i, j, water);
-			}
-		}
-		mappy.saveMap();
-		
-		// sand layer
-		double sandWidth, sandHeight;
-		BufferedImage sand = Utility.LoadImage(".\\Resources\\Sand.png");
-		for (int i = 3; i < (mappy.getWidth() - 3); i++) {
-			for (int j = 3; j < (mappy.getHeight() - 12); j++) {
-				mappy.setTile(i, j, sand);
-			}
-		}
-		mappy.saveMap();
-		
-		// ground grass layer
 	}
 }
